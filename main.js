050effa00c9cd7ac7ac599be1ae82d1c69d64c08
Student <student@RU-001.local>
2015-09-16 17:47:49 -0600

    second comment

--- main.js
+++ main.js
@@ -1,22 +1,29 @@
 angular.module('app', []);
 
-angular.module('myApp')
+angular.module('app')
 	.controller('mainController', ['$scope', function($scope){
 
 		$scope.texta = "Enter your name";
-		$scope.hideText = true
+		$scope.hideText1 = true
 
 
 		$scope.textb = "Enter your email";
 		$scope.hideText2 = true
 
+		$scope.textc = "Enter your birthday";
+		$scope.hideText3 = true
+
+
 		$scope.showInput = function(text){
 			if(text === 1 ){
-				$scope.hideText = !$scope.hideText;
+				$scope.hideText1 = !$scope.hideText1;
 			}
 			else if (text === 2) {
 				$scope.hideText2 = !$scope.hideText2;
 
+			} 
+			else if (text === 3) {
+				$scope.hideText3 = !$scope.hideText3;
 			}
 		}
 
